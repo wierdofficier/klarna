c71b72faac341868870965808c1ff44fc50e1804
wierdofficier <38385230+wierdofficier@users.noreply.github.com>
2019-06-06 11:19:56 +0200

    Update search2.c

--- search2.c
+++ search2.c
@@ -20,6 +20,10 @@
 #define RESET "\x1B[0m" 
 char search[1000][1000] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"};
 double solution[] = {9.8,3.7,8.89};
+int bufcnt = 0;
+char BIGBUF[1000][1000][3];
+char BIGBUF2[1000][1000][3];
+char BIGBUF3[1000][1000][3];
 double _count = 0;
 int iterator = 0;
 int isbreaked = 0;
@@ -275,9 +279,14 @@ char   *new_str;
 char   *strnew;
 char   *strnew2;
 char   *strnew3;
+char   *strnew4;
 char * new_str2; 
 char * new_str3;
-char * fill_equation(char * line_buf, double   *data, int lll,char * str,char *str2 )
+char * new_str4;
+int newloop = 0;
+int newloopnow = 0;
+double DATA[4][10000];
+char * fill_equation(char * line_buf, double ** data , int lll,char * str,char *str2 )
 {
 	strcpy(str, line_buf);
 	char buf1[1000];
@@ -287,7 +296,7 @@ char * fill_equation(char * line_buf, double   *data, int lll,char * str,char *s
 		if (ptr != NULL) /* Substring found */
 		{
 			//printf("%s contains '%s'\n", new_str, search[ll]);												 
-			sprintf(buf1,"%f" ,data[ll] );				 
+			sprintf(buf1,"%f" ,data[lll][ll]  );				 
 			str2 = str_replace(str,search[ll],buf1);	 
 			strcpy(str,str2); 	
 			free(str2);		 
@@ -323,6 +332,8 @@ int main()
 	strnew3  = malloc(1000);
  	new_str3  = malloc(1000);
 
+	strnew4  = malloc(1000);
+ 	new_str4  = malloc(1000);
  	char * line_buf = NULL;
  	size_t line_buf_size = 0;
 	int line_count = 0;
@@ -331,28 +342,39 @@ int main()
 	line_size = malloc(   MAXBUFLEN );	 
 	iterator = 0;
   
- 	double a = 0.00001;
-	double **VARA1;
-	VARA1 = malloc( 10000*32);
-	 
-  	for(int iiiii=0; iiiii<10000*1; iiiii++)
-	VARA1[iiiii] =  malloc(32);
- 
-	VARA1[0][0] += a;
-	VARA1[1][0] += a;
-	VARA1[2][0] += a;
+ 	double a = 0.001;
+
+
 	
-double inc  = 0;
-int counter = 0;
+
+	double inc  = 0;
+	int counter = 0;
 	while(line_size >= 0 && line_count < 600000000)
 	{
+ double **VARA1;
+	VARA1 = malloc( 100*32);
+	 
+  	for(int iiiii=0; iiiii<100*1; iiiii++)
+		VARA1[iiiii] =  malloc(1233);
+double a1[]  = {1000000.0/1000000.0 ,  1000.0/1000.0 ,    1000.0/1000.0,   1000.0/1000.0 , 1000000.0/1000000.0 ,   1000.0/1000.0, 1000.0/1000.0, 1000000.0/1000000.0,  1000.0/1000.0,  1000.0/1000.0,  1000.0/1000.0,   1000.0/1000.0   };
+		double a2[] = {1000000.0/1000000.0 , 1000.0/1000.0 ,    1000.0/1000.0,   1000.0/1000.0, 1000000.0/1000000.0,   1000.0/1000.0, 1000.0/1000.0,1000000.0/1000000.0 , 1000.0/1000.0,  1000.0/1000.0 ,  1000.0/1000.0 ,  1000.0/1000.0   };	 
+		double a3[] = {1000000.0/1000000.0 , 1000.0/1000.0 ,      1000.0/1000.0,   1000.0/1000.0,1000000.0/1000000.0,   1000.0/1000.0, 1000.0/1000.0,1000000.0/1000000.0 , 1000.0/1000.0,  1000.0/1000.0,  1000.0/1000.0,   1000.0/1000.0   };
+		double a4[] = {1000000.0/1000000.0 , 1000.0/1000.0 ,      1000.0/1000.0,   1000.0/1000.0,1000000.0/1000000.0,   1000.0/1000.0, 1000.0/1000.0,1000000.0/1000000.0 , 1000.0/1000.0,  1000.0/1000.0,  1000.0/1000.0,   1000.0/1000.0   };
 		line_size[line_count] = getline(  &line_buf  , &line_buf_size, fp5);	
-		double a1[] = {1 ,  1 ,    1,   1 ,  1,   1, 1, 1,  1,  1,  1,   1   };
-		double a2[] = {1 , 1 ,    1,   1,  1 ,   1, 1, 1 , 1,  1 ,  1 ,  1   };	 
-		double a3[] = {1 , 1 ,      1,   1,  1,   1, 1, 1 , 1,  1,  1,   1   };	 
-	       	for(int llll = 0; llll < 100000000000  ; llll++)
+		for(int oooo = 0; oooo < 4; oooo++)
 		{ 
-	 		for(int oooo = 0; oooo < 3; oooo++)
+			for(int kkk = 0; kkk < 12; kkk++)
+			{ 
+			 VARA1[oooo][kkk] = a1[kkk];
+			  VARA1[oooo][kkk] = a2[kkk];
+			 VARA1[oooo][kkk] = a3[kkk];
+			 VARA1[oooo][kkk] = a4[kkk];
+			}
+		}	 	 
+		newloop = 0;
+	       	for(int llll = 0; llll < 10*iterator  ; llll++)
+		{ 
+	 		for(int oooo = 0; oooo < 4; oooo++)
 			{ 				 
 				for(int kkk = 0; kkk < 12; kkk++)
 				{ 
@@ -363,174 +385,183 @@ int counter = 0;
 							if( (result[oooo][kkk]) >  (result[oooo][lll]))
 							{					
 								if(oooo == 0)	
-								{	
-									 a1[kkk] -= a;
+								{
+									VARA1[oooo][kkk] -= a;
 								}
 								if(oooo == 1)	
-								{
-									 a2[kkk] -= a;
+								{		 
+									VARA1[oooo][kkk] -= a;
 								}
 								if(oooo == 2)	
-								{
-									 a3[kkk] -= a;
-								}	 								 	
+								{  	  
+									VARA1[oooo][kkk] -= a;		
+								}
+								if(oooo == 3)	
+								{  	  
+									VARA1[oooo][kkk] -= a;		
+								} 								 	
 							}		
 							else
 							{				 
 								//printf("kkk = %d ::: oooo = %d\n",  kkk,oooo);									
 								if(oooo == 0)	
 								{
-									a1[kkk] += a;
+									VARA1[oooo][kkk] += a;
 								}
 								if(oooo == 1)	
 								{		 
-									a2[kkk] += a;
+									VARA1[oooo][kkk] += a;
 								}
 								if(oooo == 2)	
 								{  	  
-									a3[kkk] += a;
-									
-										
+									VARA1[oooo][kkk] += a;		
 								}
-									 	 	 	
+								if(oooo == 3)	
+								{  	  
+									VARA1[oooo][kkk] += a;		
+								}		 	 	 	
 					 		}
-
-							new_str = fill_equation(line_buf,a1,2,new_str,strnew);
+ 
+							new_str = fill_equation(line_buf,VARA1 ,0,new_str,strnew);
 							result[0][kkk] = te_interp(new_str, 0);
  
-							new_str2 = fill_equation(line_buf,a2,2,new_str2,strnew2);
+							new_str2 = fill_equation(line_buf,VARA1 ,1,new_str2,strnew2);
 							result[1][kkk] = te_interp(new_str2, 0);
 
-							new_str3 = fill_equation(line_buf,a3,2,new_str3,strnew3);
+							new_str3 = fill_equation(line_buf,VARA1 ,2,new_str3,strnew3);
 							result[2][kkk] = te_interp(new_str3, 0);
 							
-							// if((result[0][kkk] < solution[0]*1.02)  && ( result[0][kkk] > solution[0]*0.98))
-							// {
-								double diff = result[0][kkk]/result[2][kkk];
-										
-								if(diff > (9.8/8.89))	//result[2][kkk] är för liten
-								{
-									a3[kkk] += a;		
-								}
-								else
-								{
-									a3[kkk] -= a;
-								}
-								double diff2 = result[0][kkk]/result[1][kkk];
-								if(diff2 > (9.8/3.7))	//result[1][kkk] är för liten
-								{
-									a2[kkk] += a;		
-								}
-								else
-								{
-									a2[kkk] -= a;
-								}
-							
-
-								  diff = result[1][kkk]/result[2][kkk];
-										
-								if(diff > (3.7/8.89))	//result[2][kkk] är för liten
-								{
-									a3[kkk] += a;		
-								}
-								else
-								{
-									a3[kkk] -= a;
-								}
-								  diff2 = result[1][kkk] /result[0][kkk];
-								if(diff2 > ( 3.7/9.8))	//result[1][kkk] är för liten
-								{
-									a1[kkk] += a;		
-								}
-								else
-								{
-									a1[kkk] -= a;
-								}
-
-
-								 diff = result[2][kkk]/result[1][kkk];
-										
-								if(diff > (8.89/3.7))	//result[2][kkk] är för liten
-								{
-									a2[kkk] += a;		
-								}
-								else
-								{
-									a2[kkk] -= a;
-								}
-								  diff2 = result[2][kkk]/result[0][kkk];
-								if(diff2 > ( 8.89/9.8))	//result[1][kkk] är för liten
-								{
-									a1[kkk] += a;		
-								}
-								else
-								{
-									a1[kkk] -= a;
-								}
-
-							//}
-							a1[kkk] += a;
-							a2[kkk] += a;
-							a3[kkk] += a;
-
-							check_eq(oooo,kkk,new_str,new_str2,new_str3);
+							new_str4 = fill_equation(line_buf,VARA1 ,3,new_str4,strnew4);
+							result[3][kkk] = te_interp(new_str4, 0);
+ 
+							double earth_i[100][100] = {{9.8/3.7,9.8/8.89,9.8/3.7},{3.7/8.89,3.7/9.8,3.7/3.7},{8.89/3.7,8.89/9.8,8.89/3.7},{3.7/8.89,3.7/9.8,3.7/3.7}};
+		 					
+							int cc = 0;
+							int xxxc = 0;	
+ 
+							cc = 0;
+ 
+							int look[100][100] = {{2,1,3},{2,0,3},{1,0,3},{2,0,1}};
+		 					for(int xxxx = 2; xxxx <  4; xxxx++)
+							{ 
+								for(int llllll = 0; llllll <  4; llllll++)
+								{ 
+									double diff = result[xxxx][kkk]/result[look[xxxx][llllll]][kkk];
+										 
+									double ratio = earth_i[xxxx][cc];
+										 
+									if(diff > (ratio))	
+									{
+										VARA1[look[xxxx][llllll]][kkk]  += a;		
+									}
+									else
+									{
+										VARA1[look[xxxx][llllll]][kkk]  -= a;										
+									}
+										cc++;
+								}				 
+								 cc = 0;
+							} 
+ 
+							VARA1[0][kkk] += a;
+							VARA1[1][kkk] += a;
+							VARA1[2][kkk] += a;
+							VARA1[3][kkk] += a;
+							check_eq(oooo,kkk,new_str,new_str2,new_str3,new_str4);
+							if(newloop == 1)
+								break;
 							counter++;
 							if(counter % 1000  == 0)
 							{
 
-				printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",new_str, result[0][kkk],randme);
-				printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",new_str2, result[1][kkk],randme);
-				printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",new_str3, result[2][kkk],randme);
+			 	printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",new_str, result[0][kkk],randme);
+			 	printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",new_str2, result[1][kkk],randme);
+			 	printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",new_str3, result[2][kkk],randme);
 							}
 							}
+							if(newloop == 1)
+								break;
 						}
+						if(newloop == 1)
+								break;
 			 		}
+					if(newloop == 1)
+								break;
 
 				}
-	 
-		 	if(isbreaked == 1)			
-				break;
- 		 
+				for(int kkk = 0; kkk < 12; kkk++)
+				{ 
+					for(int oooo = 0; oooo < 3; oooo++)
+					{ 						
+						if(newloop == 1 || result[oooo][kkk] < 0 || result[oooo][kkk] == 0 || isnan(result[oooo][kkk]) != 0)
+						{
+							newloopnow = 1;
+							break;
+						}	
+					}	
+				}
+			
+ 		
+				if(newloopnow == 1  )
+				{
+					newloopnow = 0;
+					break;
+				}
 	 		}
 
 		line_count++;	 
 		iterator++;
 		isbreaked = 0;	
-		randme = iterator;											
-		}
-	 
+		randme = iterator;
 
+	if(bufcnt > 25)
+		break;											
+		}
+	 for(int i = 0; i < 25; i++)
+	{
+		printf("eq[%d] %s = %f\n",i,BIGBUF[i][0],te_interp(BIGBUF[i][0], 0));
+		//printf("%s\n", BIGBUF2[i][1]);
+		//printf("%s\n", BIGBUF3[i][2]);
+	}
   //fclose(fp5);
 
     return 0;
 }
 
-
-void check_eq(int oooo,int kkk, char *resultstr,char *resultstr2,char *resultstr3)
+void check_eq(int oooo,int kkk, char *resultstr,char *resultstr2,char *resultstr3,char *resultstr4)
 {
 	
 	
-	if((((( ( result[0][kkk]))) < 5.97237e24*1.03   && (( result[0][kkk])) > 5.97237e24*0.97)   || ((( result[0][kkk])) < solution[0]*1.02  && (( result[0][kkk])) > solution[0]*0.98)  || 				((( result[0][kkk])) <  5514*1.03    && (( result[0][kkk])) > 5514*0.97)  ) && isnan(( result[0][kkk])) == 0)
+	if((((( ( result[0][kkk]))) < 5.97237e24*1.03   && (( result[0][kkk])) > 5.97237e24*0.97)   || ((( result[0][kkk])) < solution[0]*1.001  && (( result[0][kkk])) > solution[0]*0.999)  || 				((( result[0][kkk])) <  1000*1.03    && (( result[0][kkk])) > 1000*0.97)  ) && isnan(( result[0][kkk])) == 0)
 	{		
  		
 		
-		if((((((result[1][11]))) < 6.4171e23*1.03   && ((result[1][11])) > 6.4171e23*0.97)   || (((result[1][11])) < solution[1]*1.02  && ((result[1][11])) > solution[1]*0.98)  || 			(((result[1][11])) <  3933.5*1.03    && ((result[1][11])) > 3933.5*0.97)  ) && isnan((result[1][11])) == 0)
+		if((((((result[1][kkk]))) < 6.4171e23*1.03   && ((result[1][kkk])) > 6.4171e23*0.97)   || (((result[1][kkk])) < solution[1]*1.001  && ((result[1][kkk])) > solution[1]*0.999)  || 			(((result[1][kkk])) <  1000.0*1.03    && ((result[1][kkk])) > 1000.0*0.97)  ) && isnan((result[1][kkk])) == 0)
 		{
 
 			printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",resultstr, result[0][kkk],randme);
 		printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",resultstr2, result[1][kkk],randme);
 		printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",resultstr3, result[2][kkk],randme);
+		printf("\neq: ="CYN" %s\n"RESET" "WHT" solution: "YEL" %f\n"RESET" "WHT" EQ linenr: "CYN" %d\n"RESET" "WHT" "RESET"\n\n",resultstr4, result[3][kkk],randme);
 		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");	
-for(;;);	
+
+		 bufcnt++;
+			sprintf(BIGBUF[bufcnt][0],"%s",resultstr);
+			sprintf(BIGBUF2[bufcnt][1],"%s",resultstr2);
+			sprintf(BIGBUF3[bufcnt][2],"%s",resultstr3);
+			printf("%s\n", BIGBUF[bufcnt][0]);
+			printf("%s\n", BIGBUF2[bufcnt][1]);
+			printf("%s\n", BIGBUF3[bufcnt][2]);
+ 		newloop = 1;	
 						//	for(int ppp = 0; ppp < 12; ppp++)
-			 			//		printf("%f ",VARA1[0][ppp] );
+			 			//		printf("%f ",a1[0][ppp] );
 							printf("\n");
 							for(int i = 0; i < 12; i++)
 							{
-				 			 //	VARA1[0][i] = a1[i]; 
-							 //	VARA1[1][i] = a2[i]; 
-							//	VARA1[2][i] = a3[i]; 
+				 			 //	a1[0][i] = a1[i]; 
+							 //	a1[1][i] = a2[i]; 
+							//	a1[2][i] = a3[i]; 
 							}
 							isbreaked = 1;
 							//break;	
