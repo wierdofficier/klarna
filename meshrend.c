d8cc00e09c8b03ff95a64c2209b36af916a2fe52
wierdofficier <38385230+wierdofficier@users.noreply.github.com>
2021-03-29 04:22:46 +0200

    Update meshrend.c

--- meshrend.c
+++ meshrend.c
@@ -54,15 +54,63 @@ void mesh_compile_list(const struct mesh *mesh, GLuint list)
 #include <stdbool.h>
  int nomoreindex = 1;
 extern int KvvVENTAL ;
-extern float *V[10];
-extern int KvvVENTALA[600000*2]; 
+extern float *V[100];
+ 
+void free_data(struct state_vector ***data, size_t xlen, size_t ylen)
+{
+    size_t i, j;
 
- extern int veclist[600000 ][2];
-extern struct state_vector   springharmony_status_link[  8333][32][1]; 
+    for (i=0; i < xlen; ++i) {
+        if (data[i] != NULL) {
+            for (j=0; j < ylen; ++j)
+                free(data[i][j]);
+            free(data[i]);
+        }
+    }
+    free(data);
+}
+ extern int veclist[8840 ][100];
+extern struct state_vector   ***  springharmony_status_link  ; 
 extern struct state_vector *** reall;
-extern struct state_vector    springharmony_status_link_[  8333][32][1];  
+extern struct state_vector    ***  springharmony_status_link_    ;  
+int ***alloc_data(size_t xlen, size_t ylen, size_t zlen)
+{
+    struct state_vector ***p;
+    size_t i, j;
 
+    if ((p = malloc(xlen * sizeof *p)) == NULL) {
+        perror("malloc 1");
+        return NULL;
+    }
 
+    for (i=0; i < xlen; ++i)
+        p[i] = NULL;
+
+    for (i=0; i < xlen; ++i)
+        if ((p[i] = malloc(ylen * sizeof *p[i])) == NULL) {
+            perror("malloc 2");
+            free_data(p, xlen, ylen);
+            return NULL;
+        }
+
+    for (i=0; i < xlen; ++i)
+        for (j=0; j < ylen; ++j)
+            p[i][j] = NULL;
+
+    for (i=0; i < xlen; ++i)
+        for (j=0; j < ylen; ++j)
+            if ((p[i][j] = malloc(zlen * sizeof *p[i][j])) == NULL) {
+                perror("malloc 3");
+                free_data(p, xlen, ylen);
+                return NULL;
+            }
+
+    return p;
+}
+ 
+int nr = 0;
+int once__ = 1;
+ 
 void mesh_calc_bounds(const struct mesh *mesh, float *min, float *max,int jj)
 {
 
@@ -79,50 +127,40 @@ double centroidz = 0;
 double cx= 0;
 double cy= 0;
 double cz= 0;
-	int i,j, nr;
+	int i,j;
+		int ll = 0;
 	const float *vbuf;
- 
 	nr = mesh_vertex_buffer(mesh, &vbuf )  /1;
- 	springharmony_status_link_[0][0][0].totnumpoints[jj] = nr;
- 	
+	if(once__ == 1)
+	{
+         springharmony_status_link_=alloc_data(7936,55,7);
+        springharmony_status_link=alloc_data(7936,55,7);
+ 
+	
+	for(  ll = 0; ll < 2 ; ll++)
+	{
+	springharmony_status_link_[0][0][0].tot_layer[ll] = 7;
 
- 
-/*
-state_result_worm_ventral= (struct state_vector ***) malloc(sizeof(struct state_vector**) * nr*3); //FIX 1
-for(i = 0; i < nr*2; i++){
- 
-  state_result_worm_ventral[i] = (struct state_vector **) malloc(sizeof(struct state_vector*) * nr*3);  // FIX 2
-  for(j = 0; j < 33; j++){
- 
-        state_result_worm_ventral[i][j] = (struct state_vector *) malloc(sizeof(struct state_vector) * 3);
-  }
-}
+	}  	
+	once__ = 0;
+	}
+	
 
+  	springharmony_status_link_[0][0][0].totnumpoints[jj] = nr/1;
  
-
-state_result_worm_ventral_feather= (struct state_vector ***) malloc(sizeof(struct state_vector**) * nr*3); //FIX 1
-for(i = 0; i < nr*2; i++){
- 
-  state_result_worm_ventral_feather[i] = (struct state_vector **) malloc(sizeof(struct state_vector*) * nr*3);  // FIX 2
-  for(j = 0; j < 33; j++){
- 
-        state_result_worm_ventral_feather[i][j] = (struct state_vector *) malloc(sizeof(struct state_vector) * 3);
-  }
-}
-*/
-
  	int kk = 0; 
   Vec3 * v;
-	int ll;
-	
+
+	ll = 0;
  
-for(kk = 0; kk < 4; kk++)
+for(kk = 0; kk < springharmony_status_link_[0][0][0].tot_layer[jj]; kk++)
 	{
-	for(ll = 0; ll < nr  ; ll++)
+	for(ll = 0; ll < springharmony_status_link_[0][0][0].totnumpoints[jj]  ; ll++)
 	{
-  printf(" index %d \n",  nr);
+ 
 	  v= (Vec3 *)(V[jj] + 3*(veclist[ll][jj]-1));
-//if(v->x != 0 && v->y != 0 && v->z != 0)
+	  
+ 
 {
 double vel1=  ((float)rand()/(float)(RAND_MAX))  ;
 double vel2=  ((float)rand()/(float)(RAND_MAX)) ;
@@ -132,58 +170,57 @@ double mass=  ((float)rand()/(float)(RAND_MAX)) ;
 if(yup == 0)
 {
          	  springharmony_status_link_[ll][0][kk].pos_new_x[jj]  = v->x/(changer1) +yup ;
- printf("vel1 =%f \n", vel1);
+ 
  	          springharmony_status_link_[ll][0][kk].pos_new_y[jj]  = v->y/(changer2)+yup ;
  		  springharmony_status_link_[ll][0][kk].pos_new_z[jj]  = v->z/(changer3) +yup ;
- 		  
+
  		   centroidx +=v->x;
 		   centroidy +=v->y;
 		   centroidz +=v->z;
+		 
  		  }
  		  else if(yup == 1)
  		  {
  		 
  		 springharmony_status_link_[ll][0][kk].pos_new_x[jj]  = (    (v->x - cx) )/changer1 + cx ;
- printf("vel1 =%f \n", vel1);
+ 
  	          springharmony_status_link_[ll][0][kk].pos_new_y[jj]  =   v->y/(changer1);
  		  springharmony_status_link_[ll][0][kk].pos_new_z[jj]  = ( (v->z - cz) )/changer3 + cz;
- 		  
- 		  
+ 		  springharmony_status_link_[ll][0][kk].springConstant[jj] =   300.1233/changer3 ;
+ 		  springharmony_status_link_[ll][0][kk].frictionConstant[jj] =   3.21233*changer3 ;
+ 		   		  
  		  }
- 		  springharmony_status_link_[ll][0][kk].mass = 1.0;
+ 		  springharmony_status_link_[ll][0][kk].mass[jj] = 1;
  
  	  	  springharmony_status_link_[ll][0][kk].vel_new_x[jj]  = 1*1e-11;
  		  springharmony_status_link_[ll][0][kk].vel_new_y[jj]  = 1*1e-11;
  		  springharmony_status_link_[ll][0][kk].vel_new_z[jj]  =1*1e-11;
 
-}
-  printf("vector   %.10f  %.10f  %.10f  %d \n", v->x,v->y,v->z,ll);
+		}
 	}
- 
 
-printf("counnn =%d\n", ll);
-	 for(ll = 0; ll < nr     ; ll++)
+	 for(ll = 0; ll < springharmony_status_link_[0][0][0].totnumpoints[jj]     ; ll++)
 	{
- 	 	 findnearestpoint(nr  ,NULL,ll,0,kk ,0.004,jj);
+ 	 	 findnearestpoint(springharmony_status_link_[0][0][0].totnumpoints[jj]  ,NULL,ll,0,kk ,0.004,jj);
 	}
 
-  changer1 = changer1 +0.015;
-  changer2 = changer2 +0.025;
-  changer3 = changer3 +0.2;
-  cx = centroidx/nr;
-  cy = centroidy/nr;
-  cz = centroidz/nr;
+  changer1 = changer1 +0.005;
+  changer2 = changer2 +0.015;
+  changer3 = changer3 +0.825 ;
+  cx = centroidx/springharmony_status_link_[0][0][0].totnumpoints[jj];
+  cy = centroidy/springharmony_status_link_[0][0][0].totnumpoints[jj];
+  cz = centroidz/springharmony_status_link_[0][0][0].totnumpoints[jj];
 printf("pos {%f,%f,%f}\n", cx,cy,cz);
 
  
   yup = 1;
 }
  
-  nomoreindex = 0;
+ 
   KvvVENTAL = 0;
 	vec_set(min, INFINITY, INFINITY, INFINITY);
 	vec_neg(max, min);
-	for (i = 0; i < nr; i++) {
+	for (i = 0; i < springharmony_status_link_[0][0][0].totnumpoints[jj]; i++) {
 		vec_min(min, min, vbuf + 3 * i);
 		vec_max(max, max, vbuf + 3 * i);
 	}
